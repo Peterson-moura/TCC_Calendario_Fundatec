62949a51b4f8370ce60c7860ef1fc61c78b60153
Michael Almeida <michaelcardoso97@gmail.com>
2022-05-04 20:22:10 -0300

    js externalizado

--- projeto_base/script.js
+++ projeto_base/script.js
@@ -1,4 +1,19 @@
 function enviar() {
-  var valor = document.getElementById("curso").value;
-  alert("Você digitou: " + valor);
+  let curso = document.getElementById("curso").value;
+  let turma = document.getElementById("turma").value;
+  let disciplina = document.getElementById("disciplina").value;
+  let professor = document.getElementById("professor").value;
+  let data = document.getElementById("data").value;
+  alert(
+    "Curso: " +
+      curso +
+      "\nTurma: " +
+      turma +
+      "\nDisciplina: " +
+      disciplina +
+      "\nProfessor(a): " +
+      professor +
+      "\nData: " +
+      data
+  );
 }
